9c0bc36681f45c2fa73cca4b498163f5deb369de
Petr Bela <bela.petr@gmail.com>
2018-11-24 00:39:45 +0700

    remove proptypes

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { PropTypes } from 'prop-types'
 import {
   DeviceEventEmitter,
   NativeModules,
@@ -31,19 +30,6 @@ export default class Player extends React.Component {
 
     return <RNWowzaPlayer ref={ref => (this._root = ref)} {...nativeProps} />
   }
-}
-
-Player.propTypes = {
-  config: PropTypes.shapeOf({
-    hostAddress: PropTypes.string.isRequired,
-    port: PropTypes.number,
-    applicationName: PropTypes.string.isRequired,
-    streamName: PropTypes.string.isRequired,
-    username: PropTypes.string,
-    password: PropTypes.string,
-  }).isRequired,
-  muted: PropTypes.bool,
-  ...View.propTypes,
 }
 
 const RNWowzaPlayer = requireNativeComponent('RNWowzaPlayer', Player)
